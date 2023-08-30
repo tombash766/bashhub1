862880a7a5780686a860f66d2e3be3f0fcb8a04d
tombash766 <87862533+tombash766@users.noreply.github.com>
2023-08-30 23:29:52 +0100

    v1.0.24

--- js/sketches.js
+++ js/sketches.js
@@ -1,23 +1,27 @@
 function sketchesLoad(){
-    fetch("/OPUserData.json").then( (Response) => Response.json() ).then( (json) => 
-        {
-            let sketches = json.sketches;
-            let c = document.getElementById("sketchesContainer");
-            for (let i = 0; i < sketches.length; i++) {
-                let currentSketch = sketches[i];
-                let newA = document.createElement("a");
-                newA.setAttribute("href", "https://openprocessing.org/sketch/" + currentSketch.visualID);
-                let newDiv = document.createElement("div");
-                newDiv.setAttribute("class", "stdHalfCol sketch");
-                newDiv.innerText = currentSketch.title;
-                let newImg = document.createElement("img");
-                newImg.setAttribute("src", "https://openprocessing-usercontent.s3.amazonaws.com/thumbnails/visualThumbnail" + currentSketch.visualID + "@2x.jpg");
-                newImg.setAttribute("width", "200px");
-                newImg.className = "sketchThumbnail"
-                newDiv.appendChild(newImg);
-                newA.appendChild(newDiv);
-                c.appendChild(newA);
-            } 
-        } 
-    )
+    fetch("/OPUserData.json")
+        .then( (Response) => {Response.json()} )
+        .then( formatSketches(json) )
+}
+
+function formatSketches(json) {
+    let sketches = json.sketches;
+    let c = document.getElementById("sketchesContainer");
+    for (let i = 0; i < sketches.length; i++) {
+        let currentSketch = sketches[i];
+        let newA = document.createElement("a");
+        newA.setAttribute("href", "https://openprocessing.org/sketch/" + currentSketch.visualID);
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "stdHalfCol sketch");
+        let newSpan = document.createElement("span");
+        newSpan.innerText = currentSketch.title;
+        let newImg = document.createElement("img");
+        newImg.setAttribute("src", "https://openprocessing-usercontent.s3.amazonaws.com/thumbnails/visualThumbnail" + currentSketch.visualID + "@2x.jpg");
+        newImg.setAttribute("width", "200px");
+        newImg.className = "sketchThumbnail";
+        newDiv.appendChild(newSpan);
+        newDiv.appendChild(newImg);
+        newA.appendChild(newDiv);
+        c.appendChild(newA);
+    } 
 }
