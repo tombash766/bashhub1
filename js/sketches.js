c8025f7ca97194273046533a379f299ec0c29677
tombash766 <87862533+tombash766@users.noreply.github.com>
2023-08-30 20:50:49 +0100

    v1.0.18

--- js/sketches.js
+++ js/sketches.js
@@ -1,15 +1,18 @@
 function sketchesLoad(){
-    fetch("/OPUserData.json").then( (Response) => Response.json() ).then( (json) => {var sketches = json.sketches} )
-    console.log(json)
-    let c = document.getElementById("sketchesContainer");
-    for (let i = 0; i < sketches.length; i++) {
-        let currentSketch = sketches[i];
-        let newA = document.createElement("a");
-        newA.setAttribute("href", "https://openprocessing.org/sketches/" + str(currentSketch.visualID));
-        let newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "stdHalfCol");
-        newDiv.setAttribute("innerText", currentSketch.title);
-        newA.appendChild(newDiv);
-        c.appendChild(newA);
-    }
+    fetch("/OPUserData.json").then( (Response) => Response.json() ).then( (json) => 
+        {
+            let sketches = json.sketches;
+            let c = document.getElementById("sketchesContainer");
+            for (let i = 0; i < sketches.length; i++) {
+                let currentSketch = sketches[i];
+                let newA = document.createElement("a");
+                newA.setAttribute("href", "https://openprocessing.org/sketches/" + str(currentSketch.visualID));
+                let newDiv = document.createElement("div");
+                newDiv.setAttribute("class", "stdHalfCol");
+                newDiv.setAttribute("innerText", currentSketch.title);
+                newA.appendChild(newDiv);
+                c.appendChild(newA);
+            } 
+        } 
+    )
 }
