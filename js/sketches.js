e103e4aed53196f71a65a96fd1160854124563b8
tombash766 <87862533+tombash766@users.noreply.github.com>
2023-08-30 20:46:05 +0100

    v1.0.18

--- js/sketches.js
+++ js/sketches.js
@@ -1,11 +1,8 @@
 function sketchesLoad(){
-    
-    fetch("/OPUserData.json").then( (Response) => Response.json() ).then( (json) => console.log(json) )
-
-    /*
+    fetch("/OPUserData.json").then( (Response) => Response.json() ).then( (json) => {let sketches = json.sketches} )
     let c = document.getElementById("sketchesContainer");
-    for (let i = 0; i < jsonObj.sketches.length; i++) {
-        let currentSketch = jsonObj.sketches[i];
+    for (let i = 0; i < sketches.length; i++) {
+        let currentSketch = sketches[i];
         let newA = document.createElement("a");
         newA.setAttribute("href", "https://openprocessing.org/sketches/" + str(currentSketch.visualID));
         let newDiv = document.createElement("div");
@@ -14,5 +11,4 @@ function sketchesLoad(){
         newA.appendChild(newDiv);
         c.appendChild(newA);
     }
-    */
 }
