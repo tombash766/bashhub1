8f8ee006ba4c6ee5a4bf2ee76da2ff4701b8ac77
tombash766 <87862533+tombash766@users.noreply.github.com>
2023-08-30 23:42:42 +0100

    v1.stupid.com

--- js/sketches.js
+++ js/sketches.js
@@ -1,10 +1,10 @@
 function sketchesLoad(){
     fetch("/OPUserData.json")
         .then( (Response) => {Response.json()} )
-        .then( (json) => {formatSketches(json)} )
+        .then( (json) => {formatSketches} )
 }
 
-function formatSketches(json) {
+const formatSketches = function(json) {
     let sketches = json.sketches;
     let c = document.getElementById("sketchesContainer");
     for (let i = 0; i < sketches.length; i++) {
