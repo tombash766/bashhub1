111c18fa47c632cfb522164e0ae5bc70426044a6
tombash766 <87862533+tombash766@users.noreply.github.com>
2023-09-01 20:53:29 +0100

    v1.0.49

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,6 @@ function galleryLoad() {
     for (let i = 1; i < 20; i++) {
         let newImg = document.createElement("img")
         newImg.setAttribute("src", "/images/gallery/" + i + ".png" )
-        newImg.setAttribute("width", "300")
         newImg.className = "galleryItem"
         c.appendChild(newImg)
     }
